1bc532c1086dc7e5823c3f358be30a7dce926f33
Hashtag-Fr <24ucs140@lnmiit.ac.in>
2026-01-11 00:56:04 +0530

    add categories in Drop down in transaction page

---
+++ utils/categories.js
@@ -0,0 +1,11 @@
+// Centralized category list for the entire app
+// Used by TransactionForm dropdown and TransactionList columns
+export const CATEGORIES = [
+  'Savings',
+  'Food',
+  'Shopping',
+  'Transport',
+  'Entertainment',
+  'Bills',
+  'Other'
+];
